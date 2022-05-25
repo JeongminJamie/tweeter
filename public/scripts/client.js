915e1ae8ca2dc6648f4f9585648e958ffe11753f
JeongminJamie <sksrhcalsu@gmail.com>
2022-05-25 19:40:50 -0400

    making form validation

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,50 +6,6 @@
 
 $(document).ready(function () {
 
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd"
-      },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ]
-
-  // const tweetData = {
-  //   "user": {
-  //     "name": "Newton",
-  //     "avatars": "https://i.imgur.com/73hZDYK.png",
-  //     "handle": "@SirIsaac"
-  //   },
-  //   "content": {
-  //     "text": "If I have seen further it is by standing on the shoulders of giants"
-  //   },
-  //   "created_at": 1461116232227
-  // }
-
-  // const $tweet = createTweetElement(tweetData);
-
-  // // Test / driver code (temporary)
-  // console.log("tweet data: ", $tweet) // to see what it looks like
-  // $('.tweets-container').append($tweet); //
-
   const createTweetElement = function (tweet) {
     let $tweet = `<article class="tweets-article">
   <header class="tweets-container-header">
@@ -63,7 +19,7 @@ $(document).ready(function () {
   <textarea name="text" class="tweets-textarea">${tweet.content.text}
   </textarea>
   <footer class="tweets-footer">
-    <span>${tweet.created_at}</span>
+    <span>${timeago.format(tweet.created_at)}</span>
     <div class="right-tweets-footer">
       <i class="fa-solid fa-flag" id="first-tweets-icon"></i>
       &nbsp
@@ -79,16 +35,37 @@ $(document).ready(function () {
   }
 
   const renderTweets = function (tweets) {
-    // loops through tweets
-
-    // calls createTweetElement for each tweet
     tweets.forEach((tweet) => {
       let $tweet = createTweetElement(tweet)
       $('.tweets-container').append($tweet);
     });
-    // takes return value and appends it to the tweets container
   }
 
-  renderTweets(data);
+  $('form').on('submit', function (event) {
+    event.preventDefault();
+
+    if ($('form textarea').val() === "" || $('form textarea').val().length > 140) {
+      alert('Please match the number of letters to the form');
+    }
+
+    const $serializedText = $(this).serialize();
+    $.ajax('/tweets/', { method: 'POST', data: $serializedText })
+  });
+
+  const loadTweets = function () {
+    $.ajax('/tweets', { method: 'GET' })
+      .done(function (response) {
+        return response.json;
+      })
+      .done(function (jsonResponse) {
+        return renderTweets(jsonResponse);
+      })
+      .fail(function (error) {
+        console.error(error);
+      })
+  }
+
+  loadTweets();
+
 });
 
